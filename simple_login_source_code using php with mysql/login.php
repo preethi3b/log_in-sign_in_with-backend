c54e75281ed3cbcf32ded28d7c4492c64f1d807b
Preethi <156524498+preethi3b@users.noreply.github.com>
2024-06-26 18:37:49 +0530

    Add files via upload

---
+++ simple_login_source_code using php with mysql/login.php
@@ -0,0 +1,102 @@
+<?php 
+session_start();
+	include("connection.php");
+	include("functions.php");
+
+
+	if($_SERVER['REQUEST_METHOD'] == "POST")
+	{
+		//something was posted
+		$user_name = $_POST['user_name'];
+		$password = $_POST['password'];
+
+		if(!empty($user_name) && !empty($password) && !is_numeric($user_name))
+		{
+
+			//read from database
+			$query = "select * from users where user_name = '$user_name' limit 1";
+			$result = mysqli_query($con, $query);
+
+			if($result)
+			{
+				if($result && mysqli_num_rows($result) > 0)
+				{
+
+					$user_data = mysqli_fetch_assoc($result);
+					
+					if($user_data['password'] === $password)
+					{
+
+						$_SESSION['user_id'] = $user_data['user_id'];
+						header("Location: index.php");
+						die;
+					}
+				}
+			}
+			
+			echo "wrong username or password!";
+		}else
+		{
+			echo "wrong username or password!";
+		}
+	}
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+</head>
+<body>
+
+	<style type="text/css">
+	body{
+		background-image:url("https://miro.medium.com/v2/resize:fit:1400/1*ZGB0kIijH91jRk5xmLgf9A.png");
+	}
+	
+	#text{
+
+		height: 25px;
+		border-radius: 5px;
+		padding: 4px;
+		border: solid thin #aaa;
+		width: 100%;
+	}
+
+	#button{
+
+		padding: 10px;
+		width: 100px;
+		color: black;
+		background-color: gray;
+		border: none;
+		margin-left: 100px;
+	}
+
+	#box{
+        margin-left: 700px;
+		background-color: palegoldenrod;
+		margin-top: 150px;
+		width: 300px;
+		padding: 20px;
+	}
+
+	</style>
+
+	<div id="box">
+		
+		<form method="post">
+			<div style="font-size: 20px;margin: 10px;color: brown; text-align:center">Login</div>
+
+			<input id="text" type="text" name="user_name" placeholder="username"><br><br>
+			<input id="text" type="password" name="password" placeholder="password"><br><br>
+
+            <input id="button" type="submit" value="Login"><br><br>
+
+			<a href="signup.php">Click to Signup</a><br><br>
+		</form>
+	</div>
+</body>
+</html>
---
+++ simple_login_source_code using php with mysql/signup.php
@@ -0,0 +1,88 @@
+<?php 
+session_start();
+
+	include("connection.php");
+	include("functions.php");
+
+
+	if($_SERVER['REQUEST_METHOD'] == "POST")
+	{
+		//something was posted
+		$user_name = $_POST['user_name'];
+		$password = $_POST['password'];
+
+		if(!empty($user_name) && !empty($password) && !is_numeric($user_name))
+		{
+
+			//save to database
+			$user_id = random_num(20);
+			$query = "insert into users (user_id,user_name,password) values ('$user_id','$user_name','$password')";
+
+			mysqli_query($con, $query);
+
+			header("Location: login.php");
+			die;
+		}else
+		{
+			echo "Please enter some valid information!";
+		}
+	}
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Signup</title>
+</head>
+<body>
+
+	<style type="text/css">
+	body{
+		background-image:url("https://miro.medium.com/v2/resize:fit:1400/1*ZGB0kIijH91jRk5xmLgf9A.png");
+	}
+	#text{
+        
+		height: 25px;
+		border-radius: 5px;
+		padding: 4px;
+		border: solid thin #aaa;
+		width: 100%;
+	}
+
+	#button{
+        
+		padding: 10px;
+		width: 100px;
+		color: black;
+		background-color: gray;
+		border: none;
+		margin-left: 100px;
+	}
+
+	#box{
+		
+        margin-left: 700px;
+		background-color:palevioletred;
+		margin-top: 150px;
+		width: 300px;
+		padding: 20px;
+	}
+
+	</style>
+
+	<div id="box">
+		
+		<form method="post">
+			<div style="font-size: 20px;margin: 10px;color: black; text-align:center;">Signup</div>
+
+			<input id="text" type="text" name="user_name"><br><br>
+			<input id="text" type="password" name="password"><br><br>
+
+			<input id="button" type="submit" value="Signup"><br><br>
+
+			<a href="login.php">Click to Login</a><br><br>
+		</form>
+	</div>
+</body>
+</html>
